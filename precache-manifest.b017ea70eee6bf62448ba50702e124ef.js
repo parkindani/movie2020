4b2cb83962c451c890123beb925ca92ea4b59ea6
danipa <danipa@paycoq.com>
2020-08-24 10:58:02 +0900

    Updates

---
+++ precache-manifest.b017ea70eee6bf62448ba50702e124ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d683e6fccef1f21a7edc162ec1e88f22",
+    "url": "/movie2020/index.html"
+  },
+  {
+    "revision": "ca95decf43b279627b07",
+    "url": "/movie2020/static/css/main.7f02c92f.chunk.css"
+  },
+  {
+    "revision": "a749c7d2e61fabe58a82",
+    "url": "/movie2020/static/js/2.cfb46a12.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie2020/static/js/2.cfb46a12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca95decf43b279627b07",
+    "url": "/movie2020/static/js/main.0794251c.chunk.js"
+  },
+  {
+    "revision": "7efdb582ded143da01d6",
+    "url": "/movie2020/static/js/runtime-main.9932bb55.js"
+  }
+]);
